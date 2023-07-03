7703e4d8d360c9cf3ded4754ae070921bbdc18aa
nitinfaujdar <nitin@squadcam.app>
2023-07-04 00:24:14 +0530

    added register and login APIs

--- ramognee/serializers.py
+++ ramognee/serializers.py
@@ -6,26 +6,61 @@ from .models import *
 
 UserModel = get_user_model()
 
-class RecruiterRegisterSerializer(serializers.ModelSerializer):
+class RegisterSerializer(serializers.ModelSerializer):
     username = serializers.CharField(required=True)
-    phone = serializers.CharField(required=True)
+    user_email = serializers.CharField(required=True)
+    user_phone_no = serializers.CharField(required=True)
     password = serializers.CharField(validators=[MinLengthValidator(8)], required=True, write_only=True)
 
     class Meta:
         model = User
-        fields = ['__all__']
+        fields = ['id', 'username', 'user_phone_no', 'user_email']
 
+    def validate_username(self, attrs):
+        user = UserModel.objects.filter(username=attrs['username'])
+        if user.exists():
+            raise serializers.ValidationError({"error_message":"This Username already exist"})
+        return attrs
+    
     def validate_phone(self, attrs):
-        user = UserModel.objects.filter(Q(phone=attrs))
+        user = UserModel.objects.filter(user_phone_no=attrs['user_phone_no'])
         if user.exists():
             raise serializers.ValidationError({"error_message":"This phone number already exist"})
         return attrs
+    
+    def validate_email(self, attrs):
+        user = UserModel.objects.filter(user_email=attrs['user_email'])
+        if user.exists():
+            raise serializers.ValidationError({"error_message":"This email ID already exist"})
+        return attrs
 
     def create(self, validated_data):
-        user = User.objects.create(
+        user = UserModel.objects.create(
             username=validated_data['username'],
             user_phone_no=validated_data['user_phone_no'],
             user_email=validated_data['user_email'],
             password=validated_data['password'],
         )
+        user = UserModel.objects.get(user_phone_no=validated_data['user_phone_no'])
+        user.set_password(validated_data['password'])
+        user.save()
         return user
+
+class LoginSerializer(serializers.ModelSerializer):
+    user_phone_no = serializers.CharField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['user_phone_no', 'password']
+    
+    def validate_user(self, attr):
+        user = UserModel.objects.filter(user_phone_no=attr['user_phone_no'])
+        if not user.exists():
+            raise serializers.ValidationError({"error_message":"User Does not exist"})
+        return attr     
+    
+    def validate_password(self, attr):
+        user = UserModel.objects.get(user_phone_no=attr['user_phone_no'])
+        if user.check_password == attr['password']:
+            raise serializers.ValidationError({"error_message":"Invalid Authentication Credentials"})
+        return attr
--- ramognee/views.py
+++ ramognee/views.py
@@ -1,3 +1,51 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status, serializers, generics, permissions
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.db.models import Q
+from .models import *
+from .serializers import *
 
 # Create your views here.
+
+UserModel = get_user_model()
+
+class RegisterView(generics.GenericAPIView):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = RegisterSerializer
+    http_method_names = ['post']
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        user = UserModel.objects.get(user_phone_no=request.data['user_phone_no'])
+        return Response({"message": "User Registered", "data": self.get_tokens(user)}, status=status.HTTP_200_OK)
+
+    def get_tokens(self, user):
+            refresh = RefreshToken.for_user(user)
+            return {
+                'user': {'phone': user.user_phone_no},
+                'access': str(refresh.access_token),
+                }
+
+class LoginView(generics.GenericAPIView):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = LoginSerializer
+    http_method_names = ['post']
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = UserModel.objects.get(user_phone_no=request.data['user_phone_no'])
+        return Response({"message": "Logged-In Successfully", "data": self.get_tokens(user)}, status=status.HTTP_200_OK)
+
+    def get_tokens(self, user):
+            refresh = RefreshToken.for_user(user)
+            return {
+                'user': {'phone': user.user_phone_no},
+                'access': str(refresh.access_token),
+                }
+    
